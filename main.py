32f7107a30991ac17879142391a53ab481d24bea
Taoista <taoista-@hotmail.com>
2022-04-21 18:19:37 -0400

    actualizacion demo de ping

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
+from pythonping import ping
 
 
 def main():
     print("iniciando el software")
+
+    
+    ip_movistar = "200.54.31.66"
+    ip_netline = "200.71.193.54"
+    ip_vtr = "no ip"
+
+
+    print("inciando el software para el testing")
+
+
+    # print(f"este es la ip de movistar => {ip_movistar}")
+    # print(f"esta es la ip de netline => {ip_netline}")
+    # print(f"esta es la ip de VTR => {ip_vtr}")
+
+
+    
+    print(ping(ip_netline))
+
+    print("------- terminndo el software de testeo-----------------")
+
+
+# movistart: 200.54.31.66
+# netline: 200.71.193.54
+# vtr: no la tengo
 
 
 if __name__ == "__main__":
---
+++ requirements.py
@@ -0,0 +1,4 @@
+ping3==4.0.3
+pytz==2022.1
+sqlparse==0.4.2
+pythonping==1.1.1
